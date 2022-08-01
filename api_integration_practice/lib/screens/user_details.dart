86f5b6d744df09c5954fa5f9d19576b665bb2f1c
Abdullah <abdullahsoomro245@gmail.com>
2022-08-01 16:15:28 +0500

    Work Update

--- api_integration_practice/lib/screens/user_details.dart
+++ api_integration_practice/lib/screens/user_details.dart
@@ -11,21 +11,30 @@ class UserDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text("User Id ${userModel.id}"),
       ),
-      body: Center(
-        child: Column(children: [
-          Text("ID: ${userModel.id}"),
-          Text("Name: ${userModel.name}"),
-          Text("UserName: ${userModel.username}"),
-          Text("Email: ${userModel.email}"),
-          Text("Address: ${userModel.address}"),
-          Text("ID: ${userModel}"),
-          Text("ID: ${userModel.id}"),
-          Text("ID: ${userModel.id}"),
-          Text("ID: ${userModel.id}"),
-          Text("ID: ${userModel.id}"),
-          Text("ID: ${userModel.id}"),
-          Text("ID: ${userModel.id}"),
-        ]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("ID: ${userModel.id}"),
+                Text("Name: ${userModel.name}"),
+                Text("UserName: ${userModel.username}"),
+                Text("Email: ${userModel.email}"),
+                Text("Location: ${userModel.phone}"),
+                Text(
+                  "Website: ${userModel.website}",
+                ),
+                Text(
+                  "Address: ${userModel.address?.city},${userModel.address?.geo?.lat},${userModel.address?.geo?.lng}, ${userModel.address?.street},${userModel.address?.suite}, ${userModel.address?.zipcode}",
+                ),
+                Text(
+                  "Company: ${userModel.company?.bs}, ${userModel.company?.catchPhrase}, ${userModel.company?.name}",
+                ),
+              ]),
+        ),
       ),
     );
   }
